ce26ce1f7382b0a694bd270df7816eeeb333e8e7
jayukasode23 <jayukasode2398@gmail.com>
2022-07-14 15:18:45 +0530

    added file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
-using uc6Apps;
-
-WagesMonth2 emp=new WagesMonth2();
-emp.WagesMonthCal2();
-
-   
-
+Console.WriteLine("Hello, World!");
